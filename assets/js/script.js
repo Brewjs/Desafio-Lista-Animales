79423beee98c02a9658fb839d2351f62a181b0aa
Nicolas Labbe <nicoo.labbe@gmail.com>
2022-03-11 23:00:41 -0300

    capturando form y creando evento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//Crear las clases en ES6 respetando la Herencia indicada en el diagrama de clases
+//Crear el método “datosPropietario” en la clase correspondiente y que pueda ser
+//accedido desde las clases inferiores.
 class Propietario {
     constructor(nombre, direccion, telefono) {
         this._nombre = nombre;
@@ -19,6 +22,7 @@ class Animal extends Propietario {
     }
 }
 
+//Crear los métodos get y set para la clase de mascota
 class Mascota extends Animal {
     constructor(nombre, direccion, telefono, tipo, nombreMascota, enfermedad) {
         super(nombre, direccion, telefono, tipo);
@@ -38,4 +42,38 @@ class Mascota extends Animal {
         this._enfermedad = enfermedad
     }
 }
+
+//Captar los elementos del formulario con JavaScript
+const capturarFormulario = document.querySelector('form');
+//Captar boton agregar formulario
+const btnAgregar = document.querySelector('#btnAgregar');
+
+const agregarMascota = (event) => {
+    event.preventDefault();
+    const dueño = document.getElementById('propietario').value;
+    const telefono = document.getElementById('telefono').value;
+    const direccion = document.getElementById('direccion').value;
+    const nombreMascota = document.getElementById('nombreMascota').value;
+    const tipo = document.getElementById('tipo').value;
+    const enfermedad = document.getElementById('enfermedad').value;
+    const resultado = document.getElementById('ul')
+
+    if (tipo === 'perro') {
+        const perro = new Mascota(dueño, telefono, direccion, nombreMascota, tipo, enfermedad);
+        resultado.innerHTML = `<li>${perro.datosPropietario()}</li> <br> <li>La mascota es de tipo: ${perro.tipo} su nombre es: ${perro.nombreMascota} y su enfermedad es: ${perro.enfermedad}</li>`;
+
+    } else if (tipo === 'gato') {
+        const gato = new Mascota(dueño, telefono, direccion, nombreMascota, tipo, enfermedad);
+        resultado.innerHTML = `<li>${gato.datosPropietario()}</li> <br> <li>La mascota es de tipo: ${gato.tipo} su nombre es: ${gato.nombreMascota} y su enfermedad es: ${gato.enfermedad}</li>`;
+    
+    } else if (tipo === 'conejo') {
+        const conejo = new Mascota(dueño, telefono, direccion, nombreMascota, tipo, enfermedad);
+        resultado.innerHTML = `<li>${conejo.datosPropietario()}</li> <br> <li>La mascota es de tipo: ${conejo.tipo} su nombre es: ${conejo.nombreMascota} y su enfermedad es: ${conejo.enfermedad}</li>`;
+    }
+   /*  btnAgregar.addEventListener('click', () =>{
+        const resultado = agregarMascota() 
+    })  */
+}
+
+capturarFormulario.addEventListener('submit', agregarMascota)
 
