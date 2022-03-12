40c3d98bbd70dbde91e4c12da08a6826c8966aa8
Nicolas Labbe <nicoo.labbe@gmail.com>
2022-03-11 21:53:08 -0300

    creacion de clases

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,41 @@
 class Propietario {
-
-    constructor(nombre, direccion, telefono){
+    constructor(nombre, direccion, telefono) {
         this._nombre = nombre;
         this._direccion = direccion;
         this._telefono = telefono
     }
-    datosPropietario(){
+    datosPropietario() {
         return (`Los datos del Propietario son: nombre: ${this._nombre}, direccion: ${this._direccion}, telefono: ${this._telefono}`)
     }
 }
 
 class Animal extends Propietario {
-
     constructor(nombre, direccion, telefono, tipo) {
-        super(nombre, direccion, telefono)
+        super(nombre, direccion, telefono);
+        this._tipo = tipo;
     }
-    get tipo (){
+    get tipo() {
         return (`El Animal es un: ${tipo}`)
     }
 }
+
+class Mascota extends Animal {
+    constructor(nombre, direccion, telefono, tipo, nombreMascota, enfermedad) {
+        super(nombre, direccion, telefono, tipo);
+        this._nombreMascota = nombreMascota;
+        this._enfermedad = enfermedad;
+    }
+    get nombreMascota() {
+        return this._nombreMascota
+    }
+    set nombreMascota(nombreMascota) {
+        this._nombreMascota = nombreMascota
+    }
+    get enfermedad() {
+        return this._enfermedad
+    }
+    set enfermedad(enfermedad) {
+        this._enfermedad = enfermedad
+    }
+}
+
